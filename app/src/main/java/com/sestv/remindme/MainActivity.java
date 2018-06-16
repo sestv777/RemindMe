97f9b58312bcfe1e7cdd02144c5f04d56cca1a72
sestv777 <sestv777@gmail.com>
2018-06-16 15:06:00 +0300

    Added Toolbar and search button.

--- app/src/main/java/com/sestv/remindme/MainActivity.java
+++ app/src/main/java/com/sestv/remindme/MainActivity.java
@@ -2,16 +2,15 @@ package com.sestv.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
 public class MainActivity extends Activity {
 
     private Toolbar toolbar;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
