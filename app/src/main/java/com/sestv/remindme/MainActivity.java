a748b8f1bf2e98baabbfbb78afe1ec713373d2fa
sestv777 <sestv777@gmail.com>
2018-06-16 14:29:25 +0300

    Create Main Activity.

--- app/src/main/java/com/sestv/remindme/MainActivity.java
+++ app/src/main/java/com/sestv/remindme/MainActivity.java
@@ -3,12 +3,31 @@ package com.sestv.remindme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
