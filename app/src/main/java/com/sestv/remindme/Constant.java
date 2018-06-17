4cf587d26b9fbcb4e8b0293c871180758147ca56
sestv777 <sestv777@gmail.com>
2018-06-17 13:38:06 +0300

    Implement navigation view and tab navigation with test fragment.

---
+++ app/src/main/java/com/sestv/remindme/Constant.java
@@ -0,0 +1,9 @@
+package com.sestv.remindme;
+
+public class Constant {
+
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+
+}
